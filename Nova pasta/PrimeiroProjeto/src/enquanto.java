f5726b550f3ed0a3e2d4dddf1e77d4d62b6b9cc4
Dev theCoxas <168282052+thecoxas@users.noreply.github.com>
2024-05-29 20:41:51 -0300

    Primeiros passos
    While

--- Nova pasta/PrimeiroProjeto/src/enquanto.java
+++ Nova pasta/PrimeiroProjeto/src/enquanto.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class enquanto {
     public static void main(String[] args) {
-        System.out.println("Welton Dias da silva");
+        
+       Scanner enter = new Scanner(System.in);
+       System.out.println("Digite seu nome completo: e depois 'S' para sair");
+       String nome = enter.nextLine();
+       while (!nome.equals("S")) {
+        System.out.println("Seja bem vindo "+ nome);
+        nome= enter.nextLine();
+       }
+            System.out.println("Fim da execuçao");
+        }
+
     }
 
-}
+
