37822656acbea693ac927cd7817c4a9b75608670
Dev theCoxas <168282052+thecoxas@users.noreply.github.com>
2024-05-30 20:20:58 -0300

    Create repetiçao.java

---
+++ Nova pasta/PrimeiroProjeto/src/repetiçao.java
@@ -0,0 +1,10 @@
+public class repetiçao {
+    public static void main(String[] args) {
+        
+        for(int cont = 0; cont < 5; cont ++){
+            System.out.println("Lucineia Rodrigues");
+        }
+        System.out.println("Acabou o loop");
+    }
+
+}
